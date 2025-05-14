598ec301818bcb888c4b373fcfc99eb48e17f54a
wanyihuang <huangwanyi398@gmail.com>
2025-05-15 00:28:30 +1000

    Changed box-shadow

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+// Followed this tutorial: https://www.codewithfaraz.com/content/361/how-to-create-a-scroll-down-button-html-css-javascript-tutorial#javascript-code
 
+document.getElementById('eButton').addEventListener('click', function() {
+  window.scrollBy({
+    behavior: 'smooth'
+  });
+});
